fff004056b1971d14d64702223c177265a405982
Bruno_Neves <nevesbruno814@gmail.com>
2025-12-28 20:56:30 -0400

    primeiro commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap().catch((err) => {
+  console.error('Error during application bootstrap:', err);
+  process.exitCode = 1;
+});
