55cc8e09c9aef3e161ac78d2fb2f22808e3d6b23
Bruno_Neves <nevesbruno814@gmail.com>
2025-12-28 22:02:03 -0400

    implementação interface IEnvConfig

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { IEnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements IEnvConfig {
+  constructor(private readonly configService: ConfigService) {
+    this.configService = configService;
+  }
+  getAppPort(): number {
+    return Number(process.env.APP_PORT || '3000');
+  }
+
+  getNodeEnv(): string {
+    return process.env.NODE_ENV || 'development';
+  }
+}
